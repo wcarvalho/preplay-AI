added aux_layers hyperparam. action_dim is input to action_encoder for observation. fixed masking for simulations in dyna + preplay. added goal + whether achievable to preplay learner log. mask out simulation losses for q_fn_subtask if no goal is achievable---still fine for main q-fn. fixed logger wrapper to handle int/float done correctly. no longer using optimistic resets wrapper. slightly cleaned up craftax_env to be simpler to read.

craftax_env.py
@@ -49,9 +49,7 @@ class StaticEnvParams:
     max_player_projectiles: int = 3
     use_precondition: bool = False
 
-dummy_achievements = jnp.zeros(
-    len(ACHIEVEMENT_REWARD_MAP)+1,
-    dtype=jnp.float32)
+dummy_achievements = jnp.zeros(len(ACHIEVEMENT_REWARD_MAP)+1, dtype=jnp.float32)
 
 def get_map_obs_shape():
     num_mob_classes = 5
@@ -443,6 +441,11 @@ def get_possible_achievements(state: EnvState, use_precondition: bool = False) -
             has_weapon
         )
     )
+    possible_achievements = jnp.concatenate((
+        possible_achievements,
+        # 1 more for health (will always be 0)
+        jnp.zeros([1], dtype=jnp.int32)
+    ))
     return possible_achievements
 
 
@@ -765,11 +768,11 @@ class CraftaxSymbolicEnvNoAutoReset(EnvironmentNoAutoReset):
         state = generate_world(world_rng, params, self.static_env_params)
 
         obs = self.get_obs(
-            state,
-            dummy_achievements,
-            dummy_achievements,
-            params,
-            jnp.zeros((), dtype=jnp.int32)  # scalar
+            state=state,
+            achievements=dummy_achievements,
+            achievement_coefficients=dummy_achievements,
+            params=params,
+            previous_action=jnp.zeros((), dtype=jnp.int32)  # scalar
         )
         return obs, state
 
@@ -780,15 +783,14 @@ class CraftaxSymbolicEnvNoAutoReset(EnvironmentNoAutoReset):
                 params: EnvParams,
                 previous_action: Optional[chex.Array] = None):
         del params
-        achievable = jnp.concatenate((
-          get_possible_achievements(state, use_precondition=self.static_env_params.use_precondition),
-          # 1 more for health (will always be 0)
-          jnp.zeros([1], dtype=jnp.int32)
-        ))
-        
+        achievable = get_possible_achievements(
+            state=state,
+            use_precondition=self.static_env_params.use_precondition)
+        assert achievable.shape == achievements.shape, 'these should have the exact same shape'
+
         task_w = jnp.concatenate(
             (achievement_coefficients,
-             jnp.zeros(len(achievable), dtype=achievement_coefficients.dtype)))
+             jnp.zeros(achievable.shape, dtype=achievement_coefficients.dtype)))
 
         return Observation(
             image=render_craftax_symbolic(state),

craftax_trainer.py
@@ -16,7 +16,7 @@ python craftax_trainer.py \
 import os
 os.environ['XLA_PYTHON_CLIENT_PREALLOCATE'] = 'false'
 
-MAX_SCORE = 226
+MAX_SCORE = 226.0
 
 
 import wandb
@@ -54,6 +54,7 @@ from jaxneurorl.wrappers import TimestepWrapper
 
 import craftax_observer
 import networks
+import alphazero_craftax
 import dyna_craftax
 import multitask_preplay_craftax_v2
 import qlearning_craftax
@@ -114,7 +115,8 @@ class LogWrapper(GymnaxWrapper):
     @partial(jax.jit, static_argnums=(0, 2))
     def reset(self, key: chex.PRNGKey, params=None):
         obs, env_state = self._env.reset(key, params)
-        state = LogEnvState(env_state, 0.0, 0, 0.0, 0, 0)
+        # NOTE: change, technically episode length is 1 here, not 0
+        state = LogEnvState(env_state, 0.0, 1, 0.0, 0, 0)
         return obs, state
 
     @partial(jax.jit, static_argnums=(0, 4))
@@ -125,22 +127,28 @@ class LogWrapper(GymnaxWrapper):
         action: Union[int, float],
         params=None,
     ):
+        """
+        # NOTE: MAJOR change from original code is that they assume that finishing an episode leads to the first state of the next episode in the same index.
+        In this codebase, when you finish an episode, you see the first state of the next episode in the NEXT index.
+        """
+        
         obs, env_state, reward, done, info = self._env.step(
             key, state.env_state, action, params
         )
         new_episode_return = state.episode_returns + reward
         new_episode_length = state.episode_lengths + 1
+        done_float = done.astype(jnp.float32)
+        done_int = done.astype(jnp.int32)
         state = LogEnvState(
             env_state=env_state,
-            episode_returns=new_episode_return * (1 - done),
-            episode_lengths=new_episode_length * (1 - done),
-            returned_episode_returns=state.returned_episode_returns * (1 - done)
-            + new_episode_return * done,
-            returned_episode_lengths=state.returned_episode_lengths * (1 - done)
-            + new_episode_length * done,
+            episode_returns=new_episode_return * (1 - done_float),
+            episode_lengths=new_episode_length * (1 - done_int),
+            returned_episode_returns=(state.returned_episode_returns * (1 - done_float)
+            + new_episode_return * done_float),
+            returned_episode_lengths=state.returned_episode_lengths * (1 - done_int)
+            + new_episode_length * done_int,
             timestep=state.timestep + 1,
         )
-
         return obs, state, reward, done, info
 
 class OptimisticResetVecEnvWrapper(object):
@@ -225,6 +233,9 @@ def craftax_experience_logger(
       trajectory: Optional[struct.PyTreeNode] = None,
       **kwargs):
 
+    ############################################################
+    # LogWrapper logging
+    ############################################################
     key_fn = lambda k: f'{k}-{num_seeds}' if num_seeds is not None else k
     main_key = key_fn(key)
     ach_key = key_fn(f'{key}-achievements')
@@ -253,11 +264,36 @@ def craftax_experience_logger(
         )
     metrics[f'{main_key}/num_actor_steps'] = train_state.timesteps
     metrics[f'{main_key}/num_learner_updates'] = train_state.n_updates
-    def callback(m):
+
+    ############################################################
+    # Observer logging
+    ############################################################
+
+    def callback(m, idx, lengths, returns):
+        # NOTE: WRONG. observation logic is wrong
+        #length_means = []
+        #return_means = []
+        #for env in range(lengths.shape[1]):
+        #  end = min(idx[env] + 1, len(lengths[:, env]))
+        #  length_means.append(lengths[:, env][:end].mean())
+        #  return_means.append(returns[:, env][:end].mean())
+
+        #key = key_fn('observer')
+
+        #m.update({
+        #    f'{key}/0.episode_length': np.mean(length_means),
+        #    f'{key}/0.episode_return': np.mean(return_means),
+        #    f'{key}/0.score': (np.mean(return_means)/MAX_SCORE)*100.0,
+        #})
         if wandb.run is not None:
           wandb.log(m)
 
-    jax.debug.callback(callback, metrics)
+    jax.debug.callback(
+       callback,
+       metrics,
+       observer_state.idx,
+       observer_state.episode_lengths,
+       observer_state.episode_returns)
 
 def make_logger(
         config: dict,
@@ -313,12 +349,17 @@ def run_single(
     else:
       raise NotImplementedError(config["ENV"])
 
-    env = TimestepWrapper(LogWrapper(env), autoreset=False)
-    vec_env = OptimisticResetVecEnvWrapper(
-        env,
-        num_envs=config["NUM_ENVS"],
-        reset_ratio=min(config["OPTIMISTIC_RESET_RATIO"], config["NUM_ENVS"]),
-    )
+    if config["OPTIMISTIC_RESET_RATIO"] == 1:
+      vec_env = env = TimestepWrapper(LogWrapper(env), autoreset=True)
+      vmap_env = True
+    else:
+      env = TimestepWrapper(LogWrapper(env), autoreset=False)
+      vec_env = OptimisticResetVecEnvWrapper(
+          env,
+          num_envs=config["NUM_ENVS"],
+          reset_ratio=min(config["OPTIMISTIC_RESET_RATIO"], config["NUM_ENVS"]),
+      )
+      vmap_env = False
 
 
     if config['ALG'] == 'qlearning':
@@ -335,7 +376,7 @@ def run_single(
         train_env_params=env_params,
         test_env_params=test_env_params,
         ObserverCls=craftax_observer.Observer,
-        vmap_env=False,
+        vmap_env=vmap_env,
       )
     elif config['ALG'] == 'qlearning_sf_aux':
       train_fn = vbb.make_train(
@@ -351,7 +392,7 @@ def run_single(
         train_env_params=env_params,
         test_env_params=test_env_params,
         ObserverCls=craftax_observer.Observer,
-        vmap_env=False,
+        vmap_env=vmap_env,
       )
     elif config['ALG'] in ['dyna']:
       train_fn = dyna_craftax.make_train(
@@ -365,7 +406,21 @@ def run_single(
         train_env_params=env_params,
         test_env_params=test_env_params,
         ObserverCls=craftax_observer.Observer,
-        vmap_env=False,
+        vmap_env=vmap_env,
+      )
+    elif config['ALG'] == 'alphazero':
+      train_fn = alphazero_craftax.make_train(
+          config=config,
+          env=vec_env,
+          model_env=env,
+          make_logger=partial(
+              make_logger,
+              learner_log_extra=alphazero_craftax.learner_log_extra
+          ),
+          train_env_params=env_params,
+          test_env_params=test_env_params,
+          ObserverCls=craftax_observer.Observer,
+          vmap_env=vmap_env,
       )
     elif config['ALG'] in ['preplay']:
       train_fn = multitask_preplay_craftax_v2.make_train(
@@ -379,7 +434,7 @@ def run_single(
         train_env_params=env_params,
         test_env_params=test_env_params,
         ObserverCls=craftax_observer.Observer,
-        vmap_env=False,
+        vmap_env=vmap_env,
       )
     elif config['ALG'] == 'pqn':
       constructor = get_pqn_fns(config)
@@ -394,7 +449,7 @@ def run_single(
         train_env_params=env_params,
         test_env_params=test_env_params,
         ObserverCls=craftax_observer.Observer,
-        vmap_env=False,
+        vmap_env=vmap_env,
       )
     elif config['ALG'] == 'usfa':
       train_fn = vbb.make_train(
@@ -409,26 +464,11 @@ def run_single(
         train_env_params=env_params,
         test_env_params=test_env_params,
         ObserverCls=craftax_observer.Observer,
-        vmap_env=False,
-      )
-    elif config['ALG'] == 'alphazero':
-      import alphazero_craftax
-      train_fn = alphazero_craftax.make_train(
-        config=config,
-        env=vec_env,
-        model_env=env,
-        make_logger=partial(
-            make_logger,
-            learner_log_extra=alphazero_craftax.learner_log_extra
-        ),
-        train_env_params=env_params,
-        test_env_params=test_env_params,
-        ObserverCls=craftax_observer.Observer,
-        vmap_env=False,
+        vmap_env=vmap_env,
       )
     else:
       raise NotImplementedError(config['ALG'])
-    
+
     start_time = time.time()
     train_vjit = jax.jit(jax.vmap(train_fn))
 
@@ -471,10 +511,9 @@ def sweep(search: str = ''):
         },
         'parameters': {
             #"ENV": {'values': ['craftax']},
-            "SEED": {'values': list(range(1,3))},
+            "SEED": {'values': list(range(1,2))},
             "NUM_ENV_SEEDS": {'values': [0]},
-            #"USE_PRECONDITION": {'values': [100., 10., 1.0, .1, .01, .001]},
-            "USE_PRECONDITION": {'values': [True]},
+            "USE_PRECONDITION": {'values': [False, True]},
         },
         'overrides': ['alg=ql', 'rlenv=craftax-1m', 'user=wilka'],
         'group': 'ql-13',
@@ -488,7 +527,7 @@ def sweep(search: str = ''):
         'parameters': {
             "ALG": {'values': ['qlearning_sf_aux']},
             #"ENV": {'values': ['craftax']},
-            "SEED": {'values': list(range(1,3))},
+            "SEED": {'values': list(range(1,2))},
             "NUM_ENV_SEEDS": {'values': [0]},
             "USE_PRECONDITION": {'values': [True, False]},
             #"AUX_COEFF": {'values': [1.0, .1, .01, .001]},
@@ -504,12 +543,8 @@ def sweep(search: str = ''):
         },
         'parameters': {
             "ALG": {'values': ['dyna']},
-            "SEED": {'values': list(range(1,3))},
+            "SEED": {'values': list(range(1,2))},
             "NUM_ENV_SEEDS": {'values': [0]},
-            "WINDOW_SIZE": {'values': [40]},
-            "MLP_LAYERS": {'values': [0, 1]},
-            "EPSILON_ANNEAL_TIME": {'values': [5e5, 2.5e5]},
-            "FIXED_EPSILON": {'values': [0, 1, 2]},
         },
         'overrides': ['alg=dyna', 'rlenv=craftax-1m', 'user=wilka'],
         'group': 'dyna-12-search',
@@ -524,12 +559,8 @@ def sweep(search: str = ''):
           "ALG": {'values': ['preplay']},
           "SEED": {'values': list(range(1,2))},
           "NUM_ENV_SEEDS": {'values': [0]},
-          "USE_PRECONDITION": {'values': [True]},
-          "WINDOW_SIZE": {'values': [20, 25, 30, 35, 40]},
-          "OFFTASK_COEFF": {'values': [1.0, .1]},
-          "NUM_OFFTASK_GOALS": {'values': [2, 5]},
-          "NUM_ACH_LAYERS": {'values': [0, 3]},
-          "SIM_EPSILON_SETTING": {'values': [1, 2, 3]},
+          "WINDOW_SIZE": {'values': [20, 30, 40]},
+          "SIMULATION_LENGTH": {'values': [10, 15, 20]},
       },
       'overrides': ['alg=preplay', 'rlenv=craftax-1m', 'user=wilka'],
       'group': 'preplay-5-search',

multitask_preplay_craftax_v2.py
@@ -84,6 +84,27 @@ def concat_start_sims(start, simulations):
     return jax.tree.map(concat_, start, simulations)
 
 
+def is_truncated(timestep):
+  non_terminal = timestep.discount
+  # either termination or truncation
+  is_last = make_float(timestep.last())
+
+  # truncated is discount=1 on AND is last
+  truncated = (non_terminal+is_last) > 1
+  return make_float(1-truncated)
+
+
+def simulation_finished_mask(initial_mask, next_timesteps):
+  # get mask
+  non_terminal = next_timesteps.discount[1:]
+  # either termination or truncation
+  is_last_t = make_float(next_timesteps.last()[1:])
+
+  # time-step of termination and everything afterwards is masked out
+  term_cumsum_t = jnp.cumsum(is_last_t, 0)
+  loss_mask_t = make_float((term_cumsum_t + non_terminal) < 2)
+  return concat_start_sims(initial_mask, loss_mask_t)
+
 def make_optimizer(config: dict) -> optax.GradientTransformation:
   num_updates = int(config["NUM_UPDATES"] + config.get("NUM_EXTRA_REPLAY", 0))
 
@@ -409,14 +430,8 @@ class MultitaskPreplayLossFn(vbb.RecurrentLossFn):
     ## Q-learning loss on batch of data
     ##################
 
-    # prepare data
-    non_terminal = data.timestep.discount
-    # either termination or truncation
-    is_last = make_float(data.timestep.last())
-
     # truncated is discount on AND is last
-    truncated = (non_terminal+is_last) > 1
-    loss_mask = make_float(1-truncated)
+    loss_mask = is_truncated(data.timestep)
 
     all_metrics = {}
     all_log_info = {
@@ -430,8 +445,8 @@ class MultitaskPreplayLossFn(vbb.RecurrentLossFn):
             target_preds=target_preds,
             actions=data.action,
             rewards=data.reward,
-            is_last=is_last,
-            non_terminal=non_terminal,
+            is_last=make_float(data.timestep.last()),
+            non_terminal=data.timestep.discount,
             loss_mask=loss_mask,
             )
         # first label online loss with online
@@ -464,12 +479,13 @@ class MultitaskPreplayLossFn(vbb.RecurrentLossFn):
             target_params=target_params)
 
         # vmap over batch
-        dyna_loss_fn = jax.vmap(dyna_loss_fn, (1, 1, 1, 1, 0), 0)
+        dyna_loss_fn = jax.vmap(dyna_loss_fn, (1, 1, 1, 1, 1, 0), 0)
         _, dyna_batch_loss, dyna_metrics, dyna_log_info = dyna_loss_fn(
                     x_t,
                     data.action,
                     h_tm1_online,
                     h_tm1_target,
+                    loss_mask,
                     jax.random.split(key_grad, B),
                 )
         batch_loss += self.dyna_coeff*dyna_batch_loss
@@ -491,6 +507,7 @@ class MultitaskPreplayLossFn(vbb.RecurrentLossFn):
     actions: jax.Array,
     h_online: jax.Array,
     h_target: jax.Array,
+    loss_mask: jax.Array,
     rng: jax.random.PRNGKey,
     params,
     target_params,
@@ -522,8 +539,9 @@ class MultitaskPreplayLossFn(vbb.RecurrentLossFn):
     timesteps = jax.tree.map(roll, timesteps)
     h_online = jax.tree.map(roll, h_online)
     h_target = jax.tree.map(roll, h_target)
+    loss_mask = jax.tree.map(roll, loss_mask)
 
-    def dyna_loss_fn_(t, a, h_on, h_tar, g, key):
+    def dyna_loss_fn_(t, a, h_on, h_tar, l_mask, any_achievable, g, key):
       """
 
       Args:
@@ -531,6 +549,8 @@ class MultitaskPreplayLossFn(vbb.RecurrentLossFn):
         a (jax.Array): [window_size] actions leading up to simulation
         h_on (jax.Array): [window_size, D] rnn-states leading up to simulation
         h_tar (jax.Array): [window_size, D] rnn-states leading up to simulation
+        l_mask (jax.Array): [window_size] loss mask
+        any_achievable (jax.Array): [] boolean indicating if any achievable goal
         g (jax.Array): [num_offtask_goals, ...] off-task goals
         key (jax.random.PRNGKey): [2], random key for simulation
       """
@@ -593,28 +613,29 @@ class MultitaskPreplayLossFn(vbb.RecurrentLossFn):
       #########################################
       # Get Off-task loss
       #########################################
-      non_terminal = all_t.discount
-      # either termination or truncation
-      is_last_t = make_float(all_t.last())
+      all_t_mask = simulation_finished_mask(l_mask, next_t)
 
-      # time-step of termination and everything afterwards is masked out
-      term_cumsum_t = jnp.cumsum(is_last_t, 0)
-      loss_mask_t = make_float((term_cumsum_t + non_terminal) < 2)
-
-      achievements = all_t.observation.achievements
+      achievements = all_t.observation.achievements.astype(jnp.float32)
+      coeff = g.astype(jnp.float32)
       # [T, K, D] * [1, K, D] --> [T, K]
-      offtask_reward = (achievements * g[None]).sum(-1)
+      offtask_reward = (achievements * coeff[None]).sum(-1)
+
       offtask_batch_td_error, offtask_batch_loss_mean, offtask_metrics, offtask_log_info = self.loss_fn(
           timestep=all_t,
           online_preds=online_preds,
           target_preds=target_preds,
           actions=all_a,
           rewards=offtask_reward,
-          is_last=is_last_t,
+          is_last=make_float(all_t.last()),
           non_terminal=all_t.discount,
-          loss_mask=loss_mask_t,
+          loss_mask=all_t_mask,
       )
 
+      # only use loss if any achievable goal
+      offtask_batch_loss_mean = offtask_batch_loss_mean * any_achievable[None]
+      offtask_batch_td_error = offtask_batch_td_error * any_achievable[None]
+      offtask_log_info['goal'] = g
+      offtask_log_info['any_achievable'] = any_achievable
       #########################################
       # run RNN + main Q-network over simulation data
       #########################################
@@ -651,9 +672,9 @@ class MultitaskPreplayLossFn(vbb.RecurrentLossFn):
           target_preds=target_preds,
           actions=all_a,
           rewards=all_t.reward,
-          is_last=is_last_t,
+          is_last=make_float(all_t.last()),
           non_terminal=all_t.discount,
-          loss_mask=loss_mask_t,
+          loss_mask=all_t_mask,
       )
 
       batch_td_error = self.offtask_coeff*jnp.abs(offtask_batch_td_error) + jnp.abs(main_batch_td_error)
@@ -665,7 +686,7 @@ class MultitaskPreplayLossFn(vbb.RecurrentLossFn):
       log_info = offtask_log_info
       return batch_td_error, batch_loss_mean, metrics, log_info
 
-    def preplay_loss_fn_(t, a, h_on, h_tar, key):
+    def preplay_loss_fn_(t, a, h_on, h_tar, l_mask, key):
       """
 
       Args:
@@ -675,8 +696,10 @@ class MultitaskPreplayLossFn(vbb.RecurrentLossFn):
         key (jax.random.PRNGKey): [2]
       """
       # sample goals available at last timestep in window
-      achievable = t.observation.achievable.astype(jnp.float32) + 1e-5
+      achievable = t.observation.achievable.astype(jnp.float32)
       achievable = jax.tree.map(lambda x: x[-1], achievable)
+      any_achievable = achievable.sum() > .1
+      achievable += 1e-5
       num_classes = t.observation.achievable.shape[-1]
       achievable = achievable/achievable.sum()
 
@@ -688,21 +711,25 @@ class MultitaskPreplayLossFn(vbb.RecurrentLossFn):
 
       # [num_offtask_goals, G]
       goals = jax.nn.one_hot(goals, num_classes=num_classes)
-      
+
       return jax.vmap(
          dyna_loss_fn_,
-         in_axes=(None, None, None, None, 0, 0)
-         )(t, a, h_on, h_tar, goals, jax.random.split(key, self.num_offtask_goals))
+         in_axes=(None, None, None, None, None, None, 0, 0)
+         )(t, a, h_on, h_tar, l_mask,
+           any_achievable,
+           goals,
+           jax.random.split(key, self.num_offtask_goals))
 
     # vmap over individual windows
     # TD ERROR: [T', num_goals, T + sim_length, num_sim]
     # Loss: [T', num_goals, num_sim, ...]
     batch_td_error, batch_loss_mean, metrics, log_info = jax.vmap(
-      preplay_loss_fn_, (1, 1, 1, 1, 0), 0)(
+      preplay_loss_fn_, (1, 1, 1, 1, 1, 0), 0)(
         timesteps,       # [T', W, ...]
         actions,         # [T', W]
         h_online,        # [T', W, D]
         h_target,        # [T', W, D]
+        loss_mask,       # [T', W]
         jax.random.split(rng, self.window_size),            # [W, 2]
       )
 
@@ -739,7 +766,7 @@ def get_in_episode(timestep):
   in_episode = (term_cumsum + non_terminal) < 2
   return in_episode
 
-from craftax.craftax.constants import Action, BLOCK_PIXEL_SIZE_IMG
+from craftax.craftax.constants import Action, BLOCK_PIXEL_SIZE_IMG, Achievement
 from craftax.craftax.renderer import render_craftax_pixels
 from visualizer import plot_frames
 
@@ -764,6 +791,8 @@ def learner_log_extra(
         q_values: np.array,
         q_loss: np.array,
         q_target: np.array,
+        goal: np.array,
+        any_achievable: np.array,
         ):
         # Extract the relevant data
         # only use data from batch dim = 0
@@ -835,8 +864,12 @@ def learner_log_extra(
         actions_taken = [Action(a).name for a in actions]
 
         def index(t, idx): return jax.tree.map(lambda x: x[idx], t)
+
+        goal_idx = goal.argmax(-1)
+        achievement = Achievement(goal_idx).name
         def panel_title_fn(timesteps, i):
-            title = f't={i}\n'
+            title = f'{achievement}. Pos: {int(any_achievable)}'
+            title += f't={i}\n'
             title += f'{actions_taken[i]}\n'
             title += f'r={timesteps.reward[i]}, $\\gamma={timesteps.discount[i]}$'
             return title
@@ -851,23 +884,29 @@ def learner_log_extra(
                 {f"learner_example/{key}/trajectory": wandb.Image(fig)})
         plt.close(fig)
 
-    def callback(d):
-        log_data(**d, key='dyna')
+    def callback(d, g, any_achievable):
+        log_data(**d, goal=g, any_achievable=any_achievable, key='dyna')
     # this will be the value after update is applied
     n_updates = data['n_updates'] + 1
     is_log_time = n_updates % config["LEARNER_EXTRA_LOG_PERIOD"] == 0
 
     if 'dyna' in data:
+      # [Batch, Env Time, Num Goals, Num Simuations]
+      goal = data['dyna'].pop('goal')
+      goal = goal[0, 0, 0, 0]
+
+      # [Batch, Env Time, Num Goals]
+      any_achievable = data['dyna'].pop('any_achievable')
+      any_achievable = any_achievable[0, 0, 0]
+
       # [Batch, Env Time, Num Goals, T+Sim, Num Simuations]
       dyna_data = jax.tree.map(lambda x: x[0, 0, 0, :, 0], data['dyna'])
 
       jax.lax.cond(
           is_log_time,
-          lambda d: jax.debug.callback(callback, d),
+          lambda d: jax.debug.callback(callback, d, goal, any_achievable),
           lambda d: None,
           dyna_data)
-
-
 
 
 class DynaAgentEnvModel(nn.Module):
@@ -995,6 +1034,7 @@ def make_agent(
           norm_type=config.get('NORM_TYPE', 'none'),
           structured_inputs=config.get('STRUCTURED_INPUTS', False),
           use_bias=config.get('USE_BIAS', True),
+          action_dim=env.action_space(env_params).n,
           ),
         rnn=rnn,
         q_fn=MLP(
@@ -1007,7 +1047,7 @@ def make_agent(
             ),
         q_fn_subtask=MLP(
             hidden_dim=config.get('Q_HIDDEN_DIM', 512),
-            num_layers=config.get('NUM_ACH_LAYERS', 1),
+            num_layers=config.get('NUM_AUX_LAYERS', 0),
             out_dim=env.action_space(env_params).n,
             activation=config['ACTIVATION'],
             activate_final=False,

networks.py
@@ -92,6 +92,7 @@ class CraftaxObsEncoder(nn.Module):
     - observation encoder: CNN over binary inputs
     - MLP with truncated-normal-initialized Linear layer as initial layer for other inputs
     """
+    action_dim: int = None
     hidden_dim: int = 512
     num_layers: int = 0
     norm_type: str = "batch_norm"
@@ -128,7 +129,7 @@ class CraftaxObsEncoder(nn.Module):
               activation=self.activation)(image, train)
 
         if obs.previous_action is not None:
-            action = jax.nn.one_hot(obs.previous_action, 43)
+            action = jax.nn.one_hot(obs.previous_action, self.action_dim or 50)
             # common trick for one-hot encodings, same as nn.Embed
             # main benefit comes from adding action
             kernel_init = nn.initializers.variance_scaling(1.0, 'fan_in', 'normal', out_axis=0)