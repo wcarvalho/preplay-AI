1dc314735797a648f50bfda1d22347d416d2db85
Wilka Carvalho <wcarvalho92@gmail.com>
2026-04-09 12:15:20 -0400

    minor

--- craftax_multigoal_trainer.py
+++ craftax_multigoal_trainer.py
@@ -503,16 +503,16 @@ def sweep(search: str = ""):
       "metric": metric,
       "parameters": {
         "ALG": {"values": ["preplay"]},
-        "FIXED_EPSILON": {"values": [1,2]},
-        "CQL_ALPHA": {"values": [0, 1e-3, 1e-4, 1e-5]},
-        "SEED": {"values": [2]},
+        "FIXED_EPSILON": {"values": [1, 2]},
+        "CQL_ALPHA": {"values": [1e-2, 1e-3, 1e-4]},
+        "SEED": {"values": [1, 2]},
         # "WINDOW_SIZE": {"values": [.5, 1.0]},
         # "NUM_PRED_LAYERS": {"values": [2, 3]},
         # "OBS_INCLUDE_GOAL": {"values": [True, False]},
         # "OPTIMISTIC_RESET_RATIO": {"values": [1]},
       },
       "overrides": ["alg=preplay_craftax", "rlenv=craftax-dyna-multigoal", "user=wilka"],
-      "group": "preplay-testing-10-fixed-sampling",
+      "group": "preplay-testing-11-shorter",
     }
   elif search == "her":
     sweep_config = {
